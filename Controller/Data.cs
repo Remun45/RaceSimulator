f9e5db513026eeaa498b89ca5264972840268a0f
Remun45 <vloedgravenremon@gmail.com>
2020-09-11 14:17:53 +0200

    adding some stuff

---
+++ Controller/Data.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Model;
+
+namespace Controller
+{
+    static class Data
+    {
+       // static Competition competition { get; set; }
+       // public static void initialize()
+       // {
+//             competition = new Competition();
+       // }
+       // static void addParticipants()
+       // {
+        //    competition.
+       // }
+    }
+}
---
+++ Model/Competition.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model
+{
+    class Competition
+    {
+        public List<IParticipant> Participants { get; set; }
+        public Queue<Track> Tracks { get; set; }
+        public Competition()
+        {
+
+        }
+        public void addParticipant(IParticipant participant)
+        {
+            Participants.Add(participant);
+        }
+    }
+
+}
